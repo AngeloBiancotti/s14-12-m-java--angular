1506d8ff45b618f14894a958c305853fe81bfd62
Alex David Frías <adavidfruiz@gmail.com>
2024-04-18 22:49:46 -0500

    add pageable

--- BACKEND/app/src/main/java/com/example/app/repository/ProductRepository.java
+++ BACKEND/app/src/main/java/com/example/app/repository/ProductRepository.java
@@ -1,9 +1,15 @@
 package com.example.app.repository;
 
+import com.example.app.model.Category;
 import com.example.app.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends CrudRepository <Product, Long> {
 
+    Page<Product> findAll(Pageable pageable);
 
 }
