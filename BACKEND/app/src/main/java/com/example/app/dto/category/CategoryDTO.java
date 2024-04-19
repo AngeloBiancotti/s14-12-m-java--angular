d955ac8ff44fc422b1c7d17bcf87772504b47530
Alex David Frías <adavidfruiz@gmail.com>
2024-04-18 22:45:23 -0500

    fix

--- BACKEND/app/src/main/java/com/example/app/dto/category/CategoryDTO.java
+++ BACKEND/app/src/main/java/com/example/app/dto/category/CategoryDTO.java
@@ -1,10 +1,15 @@
 package com.example.app.dto.category;
 
+import lombok.Getter;
+
 import java.io.Serializable;
 
 public record CategoryDTO(
+
+        @Getter
         Long id,
 
         String name
+
 ) implements Serializable {
 }
